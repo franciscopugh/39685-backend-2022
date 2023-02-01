b231f49417b861f56e6c841400ccb60e4d35485a
Francisco Pugh <franciscopugh01@gmail.com>
2023-01-31 22:19:21 -0300

    Clase 7

---
+++ clase-7/src/index.js
@@ -0,0 +1,71 @@
+/*import http from 'http'
+const PORT = 4000
+                        //Datos que entran - Datos que se devuelven
+const server = http.createServer((request, response) => {
+ response.end("Hola, este es mi primer servidor en NODE")
+})
+
+//Ejecutar servidor
+server.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
+*/
+
+import express from 'express'
+
+const app = express() //app es igual a la ejecucion de express
+const PORT = 4000
+
+const users = [
+    {
+        nombre: "Fran",
+        apellido: "Pugh",
+        id: 1,
+        cargo: "Profesor"
+    },
+    {
+        nombre: "Kevin",
+        apellido: "Quispe",
+        id: 2,
+        cargo: "Tutor"
+    },
+    {
+        nombre: "Pablo",
+        apellido: "Barbero",
+        id: 3,
+        cargo: "Tutor"
+    }
+]
+
+app.use(express.urlencoded({extended:true})) //Permite realizar consultas en la URL (req.query)
+
+app.get('/', (req,res) => {
+    res.send("Este es mi primer servidor con Express")
+})
+
+//http://localhost:4000/user?cargo=Tutor&nombre=Kevin
+app.get('/user', (req,res) => {
+    let {cargo, nombre, sueldo} = req.query
+    console.log(sueldo)
+    const usuarios = users.filter(user => user.cargo === cargo)
+    res.send(JSON.stringify(usuarios))
+    //let {limite} = req.query
+    //const arrayProducts = JSON.parse('txt')
+    //const copiaProductos = arrayProductos.slice(0, limite)
+})
+
+app.get('/user/:idUser', (req,res) => {
+    const idUser = req.params.idUser
+    const user = users.find(user => user.id === parseInt(idUser))
+    if(user) {
+        res.send(`Nombre de usuario ${user.nombre}`)
+    } else {
+        res.send(`El usuario no existe`)
+    }
+    
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`)
+})
