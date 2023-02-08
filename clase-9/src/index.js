c756851926c65ca736f4a8896d98426781525650
Francisco Pugh <franciscopugh01@gmail.com>
2023-02-07 22:35:47 -0300

    Clase 9 B

--- clase-9/src/index.js
+++ clase-9/src/index.js
@@ -1,6 +1,19 @@
 import express from "express";
 import routerProduct from "./routes/productos.routes.js";
 import { __dirname } from "./path.js";
+import multer from 'multer'
+
+//const upload = multer({dest:'src/public/img'}) Forma basica de utilizar multer
+const storage = multer.diskStorage({
+  destination: (req,file, cb) => {
+    cb(null, 'src/public/img')
+  },
+  filename: (req,file,cb) => {
+    cb(null, `${file.originalname}`)
+  }
+})
+
+const upload = multer({storage:storage})
 
 const app = express()
 const PORT = 4000 
@@ -10,8 +23,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 //Routes
-//app.use('/static', express.static(__dirname + '/public'))
+app.use('/static', express.static(__dirname + '/public'))
 app.use('/api/products', routerProduct)
+app.post('/upload',upload.single('product'), (req,res) => {
+    console.log(req.body)
+    console.log(req.file)
+    res.send("Imagen cargada")
+})
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
